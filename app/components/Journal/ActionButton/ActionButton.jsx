a29689e441fdb252e00be7cb293a18dd14812232
Blake Hagen <blakehagen@gmail.com>
2016-10-24 09:38:55 -0600

    cancel btn

--- app/components/Journal/ActionButton/ActionButton.jsx
+++ app/components/Journal/ActionButton/ActionButton.jsx
@@ -1,12 +1,28 @@
-import React from 'react'
+import React from 'react';
+import {browserHistory} from 'react-router';
 import styles from './actionButton.scss';
 
 export default class ActionButton extends React.Component {
 
   render() {
+
+    let button;
+    if (this.props.action === 'Cancel') {
+      button = (
+        <div className={styles.button} onClick={browserHistory.goBack}>
+          <p>{this.props.action}</p>
+        </div>
+      );
+    } else {
+      button = (
+        <div className={styles.button}>
+          <p>{this.props.action}</p>
+        </div>
+      );
+    }
     return (
-      <div className={styles.button}>
-        <p>{this.props.action}</p>
+      <div>
+        {button}
       </div>
     )
   }
